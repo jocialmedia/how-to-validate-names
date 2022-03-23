4f1851f1eb315c96f47d8365a1474c9f70bf7fcd
jocialmedia <githubjo@dethlefs.eu>
2022-03-23 20:19:07 +0100

    Switching hp prototype to vuejs

---
+++ app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <StartPage />
+</template>
+
+<script>
+import StartPage from './components/StartPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    StartPage
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
---
+++ app/src/components/StartPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>How to validate Names</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StartPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
