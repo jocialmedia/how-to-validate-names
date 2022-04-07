0b8389040da68da9deaf99b56de0982d027c4f9c
jocialmedia <githubjo@dethlefs.eu>
2022-04-07 14:33:18 +0200

    Add more contact info

--- app/src/views/AboutPage.vue
+++ app/src/views/AboutPage.vue
@@ -4,7 +4,8 @@
       <h1>About</h1>
 
       <h2>Impressum</h2>
-      <p>Verantwortlich: J. Leistikow, Eidelstedter Weg 103, 20255 Hamburg</p>
+      <p><b>Private non-commercial project by:</b> J. Leistikow, Eidelstedter Weg 103, 20255 Hamburg, Germany</p>
+      <p><b>Contact info:</b> <a href="https://www.jocialmedia.de">here on jocialmedia.de</a></p>
     </div>
   </div>
 </template>
